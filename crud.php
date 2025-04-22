7991c17baf490c6fe43e9e2ec29475cbf0b1f218
Mikedc <mikhael.dct@gmail.com>
2025-04-22 11:51:40 +0700

    payment

--- crud.php
+++ crud.php
@@ -37,56 +37,13 @@
         }
     }
     else if($method == 'POST'){
-        if (isset($input['name']) && isset($input['email'])) {
-            $email = $input['email'];
-            $stmt = $conn->prepare("SELECT id FROM users WHERE email = ?");
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $stmt->store_result();
-
-            // Jika email sudah ada
-            if ($stmt->num_rows > 0) {
-                echo json_encode(["message" => "Email sudah terdaftar"]);
-            } else {
-                $stmt = $conn->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
-                $stmt->bind_param("ss", $input['name'], $input['email']);
-                $stmt->execute();
-                echo json_encode(["message" => "User added", "id" => $conn->insert_id]);
-            } 
-        }else{
-                echo json_encode(["message" => "Invalid Input"]);
-            }
+        
     }
     else if($method == 'PUT'){
-        if (isset($input['id']) && isset($input['name']) && isset($input['email'])) {
-            $email = $input['email'];
-            $stmt = $conn->prepare("SELECT id FROM users WHERE email = ?");
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $stmt->store_result();
-
-            // Jika email sudah ada
-            if ($stmt->num_rows > 0) {
-                echo json_encode(["message" => "Email sudah terdaftar"]);
-            } else {
-                $stmt = $conn->prepare("UPDATE users SET name=? ,email=? WHERE id=?");
-                $stmt->bind_param("ssi", $input['name'], $input['email'], $input['id']);
-                $stmt->execute();
-                echo json_encode(["message" => "User Update, id : $input[id]"]);
-            } 
-        }else{
-            echo json_encode(["message" => "Invalid Input"]);
-        }
+        
     }
     else if($method == 'DELETE'){
-        if (isset($input['id'])) {
-            $stmt = $conn->prepare("DELETE FROM users WHERE id=?");
-            $stmt->bind_param("i", $input['id']);
-            $stmt->execute();
-            echo json_encode(["message" => "User Deleted"]);
-        } else{
-            echo json_encode(["message" => "Invalid Input"]);
-        }
+        
     }
     else{
         echo json_encode(["message" => "Method tidak di ijinkan"]);
