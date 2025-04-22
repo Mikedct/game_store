d3736233c6b4c26d1082c8dcfa7255763ac0eadf
Mikedc <mikhael.dct@gmail.com>
2025-04-22 12:55:53 +0700

    tambahin order

---
+++ order.php
@@ -0,0 +1,41 @@
+<?php
+header(header: "Content-Type: application/json");
+include "config.php";
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents("php://input"), true);
+
+if ($method == 'GET') {
+    if (isset($_GET['orderID'])) {
+        if ($_GET['orderID'] == "") {
+            echo json_encode(["message" => "ID Kosong"]);
+        } else {
+            $stmt = $conn->prepare("SELECT * FROM order Where orderID=$_GET[orderID]");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $users = $result->fetch_all(MYSQLI_ASSOC);
+            echo json_encode($users);
+        }
+    } else if (isset($_GET['title'])) {
+        if ($_GET['title'] == "") {
+            echo json_encode(["message" => "title Kosong"]);
+        } else {
+            $title = "%" . $_GET['title'] . "%";
+            $stmt = $conn->prepare("SELECT * FROM order WHERE title LIKE ?");
+            $stmt->bind_param("s", $title);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $users = $result->fetch_all(MYSQLI_ASSOC);
+            echo json_encode($users);
+        }
+    } else {
+        $stmt = $conn->prepare("SELECT * FROM order");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $users = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($users);
+    }
+} else {
+    echo json_encode(["message" => "Method tidak di ijinkan"]);
+}
+?>
