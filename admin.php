a5aedd52ccd42622597954145efbc73b0e114fc1
Mikedc <mikhael.dct@gmail.com>
2025-06-04 15:52:52 +0700

    Tambahin post

--- admin.php
+++ admin.php
@@ -83,6 +83,24 @@ if ($method == 'GET') {
         $users = $result->fetch_all(MYSQLI_ASSOC);
         echo json_encode($users);
     }
+// =====POST=====
+} else if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $dateOfBirth = $_POST['dateOfBirth'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $password = md5($_POST['password']);
+
+    $sql = "INSERT INTO admin (adminID, firstName, lastName, username, email, dateOfBirth, phoneNumber, password)
+            VALUES (NULL, '$firstName', '$lastName', '$username', '$email', '$dateOfBirth', '$phoneNumber', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["status" => "success", "message" => "Admin berhasil ditambahkan"]);
+    } else {
+        echo json_encode(["status" => "error", "message" => $conn->error]);
+    }
 
 // ===== PUT =====
 } elseif ($method == 'PUT') {
@@ -162,26 +180,6 @@ if ($method == 'GET') {
     }
 
     echo json_encode($response);
-} 
-
-// =====POST=====
-elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $dateOfBirth = $_POST['dateOfBirth'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $password = md5($_POST['password']);
-
-    $sql = "INSERT INTO admin (adminID, firstName, lastName, username, email, dateOfBirth, phoneNumber, password)
-            VALUES (NULL, '$firstName', '$lastName', '$username', '$email', '$dateOfBirth', '$phoneNumber', '$password')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(["status" => "success", "message" => "Admin berhasil ditambahkan"]);
-    } else {
-        echo json_encode(["status" => "error", "message" => $conn->error]);
-    }
 } else {
     echo json_encode(["message" => "Method not authorized"]);
 }
